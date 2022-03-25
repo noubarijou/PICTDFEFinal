f7689b22d06da2d1d1f367cd54cde0755f9257ff
jennimay <mayumi.jenni@gmail.com>
2022-03-25 10:18:31 -0300

    identação

--- alucar/src/pages/Disponibilidade/index.js
+++ alucar/src/pages/Disponibilidade/index.js
@@ -1,4 +1,4 @@
-import {Helmet} from 'react-helmet-async'
+import { Helmet } from 'react-helmet-async'
 import useAxios from '../../hooks/useAxios'
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
@@ -15,22 +15,23 @@ export const Disponibilidade = () => {
   const { width } = useWindowDimensions();
   const { detalhesId } = useParams();
   const [detalhe, setDetalhe] = useState();
+
   useEffect(() => {
     setDetalhe(detalhesId);
     window.scrollTo(0, 0)
-  }, [detalhesId])
-  
+  }, [detalhesId]);
+
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>AluCar | Disponibilidade</title>
-    </Helmet>
-    <main>
+      </Helmet>
+      <main>
         <h2>Modelos disponíveis na categoria </h2>
         <div className="categorias__filtros">
-        <p className="filtro__cidade_periodo btn-small">São Paulo, SP {` (DD/MM/YYYY ->  DD/MM/YYYY)`}</p>
-        <div className="filtro__ordernar">Ordenar</div>
-        <div className="filtro__filtrar">Filtrar</div>
+          <p className="filtro__cidade_periodo btn-small">São Paulo, SP {` (DD/MM/YYYY ->  DD/MM/YYYY)`}</p>
+          <div className="filtro__ordernar">Ordenar</div>
+          <div className="filtro__filtrar">Filtrar</div>
         </div>
         <article className="detalhe__carro">
           {detalhes[detalhesId] ? (
@@ -47,28 +48,28 @@ export const Disponibilidade = () => {
                       <figure className="carro__img">
                         <img src={e.imagens.url_imagem} alt={e.imagens.titulo} />
                       </figure>
-                    </div> 
-                    <div className="carro__caracteristica">
-                    <div className="caracteriscas__card">
-                          <CardCaracteristica icon="cambio" carcteristica="Tipo de cambio" carcteristicaDescricao={e.caracteristicas.cambio} />
-                          <CardCaracteristica icon="ar condicionado" carcteristica="Ar condicionado" carcteristicaDescricao={(e.caracteristicas.ar_condicionado) ? "Ar condicionado" : null} />
-                          <CardCaracteristica icon="assento" carcteristica="Quantidade de assento" carcteristicaDescricao={`${e.caracteristicas.qtde_assento} ${(e.caracteristicas.qtde_assento <= 1) ? "assento" : "assentos"}`} />
-                          <CardCaracteristica icon="motor" carcteristica="Motor" carcteristicaDescricao={e.caracteristicas.motor} />
-                          <CardCaracteristica icon="porta" carcteristica="Quantidade de portas" carcteristicaDescricao={`${e.caracteristicas.qtde_porta} ${(e.caracteristicas.qtde_porta <= 1) ? "porta" : "portas"}`} />
                     </div>
+                    <div className="carro__caracteristica">
+                      <div className="caracteriscas__card">
+                        <CardCaracteristica icon="cambio" carcteristica="Tipo de cambio" carcteristicaDescricao={e.caracteristicas.cambio} />
+                        <CardCaracteristica icon="ar condicionado" carcteristica="Ar condicionado" carcteristicaDescricao={(e.caracteristicas.ar_condicionado) ? "Ar condicionado" : null} />
+                        <CardCaracteristica icon="assento" carcteristica="Quantidade de assento" carcteristicaDescricao={`${e.caracteristicas.qtde_assento} ${(e.caracteristicas.qtde_assento <= 1) ? "assento" : "assentos"}`} />
+                        <CardCaracteristica icon="motor" carcteristica="Motor" carcteristicaDescricao={e.caracteristicas.motor} />
+                        <CardCaracteristica icon="porta" carcteristica="Quantidade de portas" carcteristicaDescricao={`${e.caracteristicas.qtde_porta} ${(e.caracteristicas.qtde_porta <= 1) ? "porta" : "portas"}`} />
+                      </div>
                     </div>
                     <div className="carro__preco_reserva">
-                    <p className="subtitle">A partir de</p>
-                    <p className="info__preco">R$ {e.valor} / dia</p>
-                    <Reservar />
+                      <p className="subtitle">A partir de</p>
+                      <p className="info__preco">R$ {e.valor} / dia</p>
+                      <Reservar />
                     </div>
                   </div>
                 );
-                })}
+              })}
             </>
-            ) : (<Spinner />)}
+          ) : (<Spinner />)}
         </article>
-    </main>
+      </main>
     </>
   )
 }
