aa4e46ca7fa4b76ffb43d3e740803ecca54986d7
jennimay <mayumi.jenni@gmail.com>
2022-03-29 22:21:58 -0300

    DateRangePicker finalizado

---
+++ alucar/src/components/ModalPesquisa/components/Calendar/index.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import "./style.scss";
+import { useWindowDimensions } from '../../../../hooks/useWindowDimensions';
+
+/* Font awesome */
+/* icones - font awesome */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDay } from "@fortawesome/free-solid-svg-icons";
+
+/* MUI */
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import MobileDateRangePicker from '@mui/lab/MobileDateRangePicker';
+import DesktopDateRangePicker from '@mui/lab/DesktopDateRangePicker';
+import TextField from '@mui/material/TextField';
+
+/* Date-fns */
+import isWeekend from 'date-fns/isWeekend';
+
+export const Calendar = ({value, setValue}) => {
+    const { width } = useWindowDimensions();
+
+    /* const [value, setValue] = useState([null, null]); */
+    /* console.log(value) */
+
+    return (
+        <div className="date__pesquisa">
+            <label>
+                <FontAwesomeIcon icon={faCalendarDay} />
+                Quando quer retirar o carro?
+            </label>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                {width < 768 ?
+                    <>
+                        <MobileDateRangePicker
+                            disablePast
+                            startText="Check-in"
+                            endText="Check-out"
+                            toolbarTitle="Período de locação"
+                            inputFormat='dd/MM/yyyy'
+                            shouldDisableDate={isWeekend}
+                            value={value}
+                            onChange={(newValue) => {
+                                setValue(newValue);
+                            }}
+                            renderInput={(startProps, endProps) => {
+                                
+                                return (
+                                    <>
+                                        <div className="date__div">
+                                            <input className="date__input start" ref={startProps.inputRef} {...startProps.inputProps} placeholder="Check-in" required />
+                                            <input className="date__input end" ref={endProps.inputRef} {...endProps.inputProps} placeholder="Check-out" required />
+                                        </div>
+                                    </>
+                                )
+                            }}
+                        />
+                    </>
+                    :
+                    <DesktopDateRangePicker
+                        disablePast
+                        shouldDisableDate={isWeekend}
+                        value={value}
+                        onChange={(newValue) => {
+                            setValue(newValue);
+                        }}
+                        renderInput={(startProps, endProps) => (
+                            <>
+                                <div className="date__div">
+                                    <input className="date__input start" ref={startProps.inputRef} {...startProps.inputProps} placeholder="Check-in" required/>
+                                    <input className="date__input end" ref={endProps.inputRef} {...endProps.inputProps} placeholder="Check-out" required />
+                                </div>
+                            </>
+                        )}
+                    />
+
+                }
+            </LocalizationProvider>
+
+        </div>
+
+    )
+}
--- alucar/src/components/ModalPesquisa/index.js
+++ alucar/src/components/ModalPesquisa/index.js
@@ -1,34 +1,38 @@
 import "./style.scss";
-import {useAxios} from "../../hooks/useAxios";
+import { useAxios } from "../../hooks/useAxios";
 /* icones - font awesome */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faLocationDot,
-  faCalendarDay,
-} from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Calendar } from "./components/Calendar";
+import { useNavigate } from "react-router-dom";
 
 export const ModalPesquisa = () => {
   const cidades = useAxios(`/cidades`);
   const [dateRange, setDateRange] = useState([null, null]);
   const [startDate, endDate] = dateRange;
   const [cidade, setCidade] = useState();
-  
+  const navigate = useNavigate();
 
-  localStorage.setItem("dadosCidade", JSON.stringify(cidade));
-  localStorage.setItem("dadosRange", JSON.stringify(dateRange));
-  localStorage.setItem("dadosStartDate", JSON.stringify(startDate));
-  localStorage.setItem("dadosEndDate", JSON.stringify(endDate));
-  const pesquisaCidade = localStorage.getItem("dadosCidade");
+  const setDados = () => {
+    localStorage.setItem("dadosCidade", JSON.stringify(cidade));
+    localStorage.setItem("dadosRange", JSON.stringify(dateRange));
+    localStorage.setItem("dadosStartDate", JSON.stringify(startDate));
+    localStorage.setItem("dadosEndDate", JSON.stringify(endDate));
+
+    /* navigate("/disponibilidade/:detalhesId"); */
+
+  }
+
+  /* const pesquisaCidade = localStorage.getItem("dadosCidade");
   const pesquisaRange = localStorage.getItem("dadosRange");
   const pesquisaStartDate = localStorage.getItem("dadosStartDate");
-  const pesquisaEndDate = localStorage.getItem("dadosEndDate");  
+  const pesquisaEndDate = localStorage.getItem("dadosEndDate"); */
   return (
     <div className="modal__pesquisa">
       <h2>Os melhores veículos, pelo melhor preço!</h2>
       <form className="pesquisa__form">
-        <div>
+        <div className="pesquisa__div">
           <label htmlFor="where">
             <FontAwesomeIcon icon={faLocationDot} /> Onde quer retirar o carro:
           </label>
@@ -53,21 +57,15 @@ export const ModalPesquisa = () => {
             })}
           </datalist>
         </div>
-        <div>
-          <label htmlFor="whenStart">
-            <FontAwesomeIcon icon={faCalendarDay} /> Período de locação:
-          </label>
-          
-          {/* <input type="date" id="whenStart" placeholder="Selecione a data inicial de locação" required /> */}
+        <div className="pesquisa__div">
+          <Calendar value={dateRange} setValue={setDateRange} />
         </div>
-        <Link to={`/disponibilidade/}`}>
           <button
             type="submit"
-            className="btn secondary-btn btn-large"  /* onSubmit={dadosPesquisa} */
+            className="btn secondary-btn btn-large" onClick={setDados()}
           >
             Pesquisar
           </button>
-        </Link>
       </form>
     </div>
   );
