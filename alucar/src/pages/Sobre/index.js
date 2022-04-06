081bc66aa19879abbefad301cf007a6056334b47
jennimay <mayumi.jenni@gmail.com>
2022-04-06 18:31:51 -0300

    correção do estilo da página sobre

--- alucar/src/pages/Sobre/index.js
+++ alucar/src/pages/Sobre/index.js
@@ -12,13 +12,15 @@ export const Sobre = () => {
         <title>AluCar | Sobre</title>
       </Helmet>
 
-      <main className="page__about">
-        <section className="section__about">
-          <h1> Nossa História</h1>
-          <p className="body-large">Somos a plataforma de aluguel de carros online que conecta quem precisa alugar um carro com as melhores opções e preços do país.</p>
-          <p className="body-large">Fundada em 2022 por um grupo de amigos apaixonados por tecnologia que sonhavam em revolucionar o mercado de carros por aluguel, esse sonho hoje está transformando a maneira como se aluga carros online.</p>
-          <p className="body-large">A Alucar veio para desburocratizar o serviço de locação de automóveis, simplificando todo o processo para atender a necessidade do cliente, pois acreditamos que quem precisa alugar um carro tem pressa.</p>
-          <p className="body-large">Nossa plataforma 100% segura traz a segurança e conforto necessários para utilização dos nossos serviços, assim como nossas condições imperdíveis que se adequam ao seu bolso.</p>
+      <main>
+        <section className="about__section">
+          <article className='about__article'>
+            <h1> Nossa História</h1>
+            <p className="body-large">Somos a plataforma de aluguel de carros online que conecta quem precisa alugar um carro com as melhores opções e preços do país.</p>
+            <p className="body-large">Fundada em 2022 por um grupo de amigos apaixonados por tecnologia que sonhavam em revolucionar o mercado de carros por aluguel, esse sonho hoje está transformando a maneira como se aluga carros online.</p>
+            <p className="body-large">A Alucar veio para desburocratizar o serviço de locação de automóveis, simplificando todo o processo para atender a necessidade do cliente, pois acreditamos que quem precisa alugar um carro tem pressa.</p>
+            <p className="body-large">Nossa plataforma 100% segura traz a segurança e conforto necessários para utilização dos nossos serviços, assim como nossas condições imperdíveis que se adequam ao seu bolso.</p>
+          </article>
           <Perfil />
         </section>
       </main>
