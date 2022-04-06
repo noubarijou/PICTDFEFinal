7013d3c24657a640b8f1e02e2ac284fe3623b9cc
jennimay <mayumi.jenni@gmail.com>
2022-04-06 18:03:48 -0300

    Página 404 criada

--- alucar/src/pages/NotFound/index.js
+++ alucar/src/pages/NotFound/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import './style.scss'
+
+/* Font Awesome */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCarBurst } from "@fortawesome/free-solid-svg-icons"
 
 export const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
+    return (
+        <main>
+            <div className='notFound__container'>
+                <FontAwesomeIcon icon={faCarBurst} size="10x" className='notFound__icon'/>
+                <h2 className='notFound__message'>Sorry page Not Found</h2>
+            </div>
+        </main>
+    )
 }
