176b054b12b67cef94d5a78053213deb1630a52c
noubarijou <85699669+noubarijou@users.noreply.github.com>
2022-04-07 08:22:51 -0300

    corrige nome do icone (fontawesome locao)

--- alucar/src/pages/NotFound/index.js
+++ alucar/src/pages/NotFound/index.js
@@ -3,13 +3,13 @@ import './style.scss'
 
 /* Font Awesome */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCarBurst } from "@fortawesome/free-solid-svg-icons"
+import  {faCarCrash}  from "@fortawesome/free-solid-svg-icons"
 
 export const NotFound = () => {
     return (
         <main>
             <div className='notFound__container'>
-                <FontAwesomeIcon icon={faCarBurst} size="10x" className='notFound__icon'/>
+                <FontAwesomeIcon icon={faCarCrash} size="10x" className='notFound__icon'/>
                 <h2 className='notFound__message'>Sorry page Not Found</h2>
             </div>
         </main>
