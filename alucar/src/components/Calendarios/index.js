a8f99dc12823f8550724b6ab4d3b56345e774acb
jennimay <mayumi.jenni@gmail.com>
2022-04-19 00:28:48 -0300

    Unindo todos os calendários em um único arquivo

--- alucar/src/components/Calendarios/index.js
+++ alucar/src/components/Calendarios/index.js
@@ -1,3 +1,4 @@
+import './style.scss';
 import * as React from 'react';
 import { useWindowDimensions } from '../../hooks/useWindowDimensions';
 
@@ -5,11 +6,17 @@ import { useWindowDimensions } from '../../hooks/useWindowDimensions';
 import StaticDateRangePicker from '@mui/lab/StaticDateRangePicker';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import MobileDateRangePicker from '@mui/lab/MobileDateRangePicker';
+import DesktopDateRangePicker from '@mui/lab/DesktopDateRangePicker';
+
+/* Font awesome */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDay } from "@fortawesome/free-solid-svg-icons";
 
 /* Date-fns */
 import isWeekend from 'date-fns/isWeekend';
 
-export const Calendario = ({ value, setValue }) => {
+export const CalendarStatic = ({ value, setValue }) => {
   const { width } = useWindowDimensions();
 
   return (
@@ -35,4 +42,67 @@ export const Calendario = ({ value, setValue }) => {
       />
     </LocalizationProvider>
   );
+}
+
+
+export const Calendar = ({value, setValue}) => {
+  const { width } = useWindowDimensions();
+
+  return (
+      <div className="date__pesquisa">
+          <label>
+              <FontAwesomeIcon icon={faCalendarDay} />
+              Quando quer retirar o carro?
+          </label>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+              {width < 768 ?
+                  <>
+                      <MobileDateRangePicker
+                          disablePast
+                          startText="Check-in"
+                          endText="Check-out"
+                          toolbarTitle="Período de locação"
+                          inputFormat='dd/MM/yyyy'
+                          shouldDisableDate={isWeekend}
+                          value={value}
+                          onChange={(newValue) => {
+                              setValue(newValue);
+                          }}
+                          renderInput={(startProps, endProps) => {
+                              
+                              return (
+                                  <>
+                                      <div className="date__div">
+                                          <input className="date__input start" ref={startProps.inputRef} {...startProps.inputProps} placeholder="Check-in" required />
+                                          <input className="date__input end" ref={endProps.inputRef} {...endProps.inputProps} placeholder="Check-out" required />
+                                      </div>
+                                  </>
+                              )
+                          }}
+                      />
+                  </>
+                  :
+                  <DesktopDateRangePicker
+                      disablePast
+                      shouldDisableDate={isWeekend}
+                      value={value}
+                      onChange={(newValue) => {
+                          setValue(newValue);
+                      }}
+                      renderInput={(startProps, endProps) => (
+                          <>
+                              <div className="date__div">
+                                  <input className="date__input start" ref={startProps.inputRef} {...startProps.inputProps} placeholder="Check-in" required/>
+                                  <input className="date__input end" ref={endProps.inputRef} {...endProps.inputProps} placeholder="Check-out" required />
+                              </div>
+                          </>
+                      )}
+                  />
+
+              }
+          </LocalizationProvider>
+
+      </div>
+
+  )
 }
