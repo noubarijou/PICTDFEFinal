a275161dbf4dba02db443d60aaf284ff8791dc9f
jennimay <mayumi.jenni@gmail.com>
2022-04-19 00:26:59 -0300

    mudando para pt-br

--- alucar/src/pages/NotFound/index.js
+++ alucar/src/pages/NotFound/index.js
@@ -10,7 +10,7 @@ export const NotFound = () => {
         <main>
             <div className='notFound__container'>
                 <FontAwesomeIcon icon={faCarCrash} size="10x" className='notFound__icon'/>
-                <h2 className='notFound__message'>Sorry page Not Found</h2>
+                <h2 className='notFound__message'>Página não encontrada</h2>
             </div>
         </main>
     )
